408507250f17262a50f0cc64209005948ed46345
KMzKINGth007 <151488513+KMzKINGth007@users.noreply.github.com>
2023-12-25 19:44:50 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,59 @@
+const root = ReactDOM.createRoot(document.querySelector('#root'));
+
+
+function Counter({ id, onDelete }) {
+  const [countNum, setCountNum] = React.useState(0);
+
+  const updateCounter = (n) => {
+    if (countNum + n < 0) {
+      return;
+    }
+    setCountNum(countNum + n);
+  };
+  const delCounter = () => {
+  };
+
+  return (
+    <div className="counter">
+      <button className="btn-inc" onClick={() => updateCounter(1)}>+</button>
+      <h3 className="number">{countNum}</h3>
+      <button className="btn-dec" onClick={() => updateCounter(-1)}>-</button>
+      <button className="btn-clr" onClick={() => updateCounter(-countNum)}>C</button>
+      <button className="btn-del" onClick={() => onDelete(id)}>X</button>
+    </div>
+  );
+}
+
+function App() {
+  const [counters, setCounters] = React.useState([]);
+
+  const addCounter = () => {
+    setCounters([...counters, { id: counters.length }]);
+    totalCounter++;
+  };
+
+  const deleteCounter = (id) => {
+    setCounters((prevCounters) => prevCounters.filter((counter) => counter.id !== id));
+  };
+
+  const getCounterSum = () => {
+    return counters.length;
+  };
+
+  return (
+    <>
+      <div className="containner">
+      <h1 className="show-sum">Sum = {getCounterSum()}</h1>
+        <button className="btn-add" onClick={addCounter}>Add Counter</button>
+        <div className="item">
+          {counters.map((counter) => (
+            <div key={counter.id}>
+              <Counter id={counter.id} onDelete={deleteCounter} />
+            </div>
+          ))}
+        </div>
+      </div>
+    </>
+  );
+}
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);
